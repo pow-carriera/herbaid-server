2ac169f59bb08b1536b1475daf58fd64cf869621
Paulo <paulo.pertierra@gmail.com>
2023-01-01 00:36:49 +0800

    Push to GitHub

---
+++ src/api/remedy/controllers/remedy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * remedy controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::remedy.remedy');
---
+++ src/api/remedy/routes/remedy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * remedy router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::remedy.remedy');
